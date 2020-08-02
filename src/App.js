5583fe30b7d39743f95c2cfb898bdff8d94362c9
Chris Williams <cwill833@gmail.com>
2020-08-02 11:42:39 -0500

    create landing page structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import styles from './App.module.scss'
 function App() {
   return (
-    <div>
-      <h1>Pulice Williams</h1>
+    <div className={styles.landingContainer}>
+      <div className={styles.menuPlusContent}>
+        <div>SideBar Menu</div>
+        <div>middle content (this will be a switch component)</div>
+      </div>
+      <div>footer component</div>
     </div>
   );
 }
