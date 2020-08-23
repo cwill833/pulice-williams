f98d0a249b896ae01900389bb0c1bb523e895a67
Matt Sartain <matthewsartain@me.com>
2020-08-23 12:48:55 -0500

    minimal style added to footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Footer.module.scss';
+import styles from './Footer.module.scss';
 
 const Footer = (props) => {
     return (
-        <div>
-            <Link to='/restaurants' className='restaurants footer-link'>
-                <img className='link-image' src='' alt=''></img>
+        <div className={styles.footer}>
+            <Link to='/restaurants' className={styles.footerLink}>
+                <img className='linkImage' src='https://fillmurray.com/g/200/300' alt=''></img>
                 <span>RESTAURANTS</span>
             </Link>
-            <Link to='/offices' className='offices footer-link'>
-                <img className='link-image' src='' alt=''></img>
+            <Link to='/offices' className={styles.footerLink}>
+                <img className='linkImage' src='https://fillmurray.com/g/200/300' alt=''></img>
                 <span>OFFICES</span>
             </Link>
-            <Link to='/residential' className='residential footer-link'>    
-                <img className='link-image' src='' alt=''></img>
+            <Link to='/residential' className={styles.footerLink}>    
+                <img className='linkImage' src='https://fillmurray.com/g/200/300' alt=''></img>
                 <span>RESIDENTIAL</span>
             </Link>
-            <Link to='/retail' className='retail footer-link'>
-                <img className='link-image' src='' alt=''></img>
+            <Link to='/retail' className={styles.footerLink}>
+                <img className='linkImage' src='https://fillmurray.com/g/200/300' alt=''></img>
                 <span>RETAIL</span>
                 </Link>
-            <Link to='/current' className='current footer-link'>
-                <img className='link-image' src='' alt=''></img>
+            <Link to='/current' className={styles.footerLink}>
+                <img className='linkImage' src='https://fillmurray.com/g/200/300' alt=''></img>
                 <span>CURRENT</span>
             </Link>
         </div>
